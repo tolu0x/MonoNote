8a748aaee0e0cdb6c570082f90b9bc9a9efa229f
Tolu <kalejaiyetoluwalase204@gmail.com>
2023-04-27 09:35:45 +0100

    refactored Reviews component

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import stars from "../assets/stars-icon.png";
-import quoteIcon from "../assets/quote-icon.png";
-import twtIcon from "../assets/twitter-icon.png";
-import tobi from "../assets/tobi.png";
-import tomiwa from "../assets/tomiwa.png";
-import timi from "../assets/timi.png";
-import ayomide from "../assets/ayomide.png";
 import playStore from "../assets/play-store.svg"
 import appStore from "../assets/app-store.svg"
 import leftDoodle from "../assets/left-doodle.png";
 import rightDoodle from "../assets/right-doodle.png";
 
-
+const reviewsData = [
+  {
+    id: 1,
+    name: "Ayomide J.",
+    username: "@ayodesigns",
+    body: "This app has changed the way I organize my life. I don’t know how I lived without it. I love MonoNote.",
+    clientImg: new URL(`../assets/ayomide.png`, import.meta.url).href,
+    quoteIcon: new URL(`../assets/quote-icon.png`, import.meta.url).href,
+    twtIcon: new URL(`../assets/twitter-icon.png`, import.meta.url).href,
+  },
+  {
+    id: 2,
+    name: "Timilehin A.",
+    username: "@inqtimi",
+    body: "As a busy professional, I depend on MonoNote to keep me organized and productive. Highly recommend!",
+    clientImg: new URL(`../assets/timi.png`, import.meta.url).href,
+    quoteIcon: new URL(`../assets/quote-icon.png`, import.meta.url).href,
+    twtIcon: new URL(`../assets/twitter-icon.png`, import.meta.url).href,
+  },
+  {
+    id: 3,
+    name: "Tobi Williams",
+    username: "@techbroke",
+    body: "With MonoNote, I can capture all my ideas in one place and stay organized with tags and categories.",
+    clientImg: new URL(`../assets/tobi.png`, import.meta.url).href,
+    quoteIcon: new URL(`../assets/quote-icon.png`, import.meta.url).href,
+    twtIcon: new URL(`../assets/twitter-icon.png`, import.meta.url).href,
+  },
+  {
+    id: 4,
+    name: "Tomiwa P.",
+    username: "@figbaba",
+    body: "The user interface is simple and easy to navigate, and the automatic syncing across all my devices is a lifesaver.",
+    clientImg: new URL(`../assets/tomiwa.png`, import.meta.url).href,
+    quoteIcon: new URL(`../assets/quote-icon.png`, import.meta.url).href,
+    twtIcon: new URL(`../assets/twitter-icon.png`, import.meta.url).href,
+  },
+];
 const Reviews = () => {
   return (
     <section className="py-6 border-b-2 border-black relative lg:h-[60rem] md:h-[75rem] sm:h-[113rem]">
@@ -23,93 +54,30 @@ const Reviews = () => {
       </div>
 
       <div className="flex items-center justify-center gap-3 px-3 pt-5 pb-20 lg:flex-nowrap md:flex-wrap sm:flex-col">
-        <div className="border border-black p-4 w-1/4 h-60 rounded-md gap-4 flex flex-col justify-center items-start md:w-2/5 sm:w-full">
-          <img src={quoteIcon} />
+        {
+          reviewsData.map((review) => {
+            return (
+              <div className="border border-black p-4 w-1/4 h-60 rounded-md gap-4 flex flex-col justify-center items-start md:w-2/5 sm:w-full">
+                <img src={review.quoteIcon} />
 
-          <p className="w-full">
-            This app has changed the way I organize my life. I don’t know how I
-            lived without it. I love MonoNote.
-          </p>
+                <p className="w-full">{review.body}</p>
 
-          <div className="flex items-center justify-between w-full">
-            <div className="flex items-center justify-center gap-2">
-              <img src={ayomide} />
+                <div className="flex items-center justify-between w-full">
+                  <div className="flex items-center justify-center gap-2">
+                    <img src={review.clientImg} />
 
-              <div className="flex flex-col items-start justify-center">
-                <p className="font-medium">Ayomide J.</p>
-                <p>@ayodesigns</p>
+                    <div className="flex flex-col items-start justify-center">
+                      <p className="font-medium">{review.name}</p>
+                      <p>review.username</p>
+                    </div>
+                  </div>
+
+                  <img src={review.twtIcon} />
+                </div>
               </div>
-            </div>
-
-            <img src={twtIcon} />
-          </div>
-        </div>
-
-        <div className="border border-black p-4 w-1/4 h-60 rounded-md gap-4 flex flex-col justify-center items-start md:w-2/5 sm:w-full">
-          <img src={quoteIcon} />
-
-          <p className="w-full">
-            As a busy professional, I depend on MonoNote to keep me organized
-            and productive. Highly recommend!
-          </p>
-
-          <div className="flex items-center justify-between w-full">
-            <div className="flex items-center justify-center gap-2">
-              <img src={timi} />
-
-              <div className="flex flex-col items-start justify-center">
-                <p className="font-medium">Timilehin A.</p>
-                <p>@inqtimi</p>
-              </div>
-            </div>
-
-            <img src={twtIcon} />
-          </div>
-        </div>
-
-        <div className="border border-black p-4 w-1/4 h-60 rounded-md gap-4 flex flex-col justify-center items-start md:w-2/5 sm:w-full">
-          <img src={quoteIcon} />
-
-          <p className="w-full">
-            With MonoNote, I can capture all my ideas in one place and stay
-            organized with tags and categories.
-          </p>
-
-          <div className="flex items-center justify-between w-full">
-            <div className="flex items-center justify-center gap-2">
-              <img src={tobi} />
-
-              <div className="flex flex-col items-start justify-center">
-                <p className="font-medium">Tobi Williams</p>
-                <p>@techbroke</p>
-              </div>
-            </div>
-
-            <img src={twtIcon} />
-          </div>
-        </div>
-
-        <div className="border border-black p-4 w-1/4 h-60 rounded-md gap-4 flex flex-col justify-center items-start md:w-2/5 sm:w-full">
-          <img src={quoteIcon} />
-
-          <p className="w-full">
-            The user interface is simple and easy to navigate, and the automatic
-            syncing across all my devices is a lifesaver.
-          </p>
-
-          <div className="flex items-center justify-between w-full">
-            <div className="flex items-center justify-center gap-2">
-              <img src={tomiwa} />
-
-              <div className="flex flex-col items-start justify-center">
-                <p className="font-medium">Tomiwa P.</p>
-                <p>@figbaba</p>
-              </div>
-            </div>
-
-            <img src={twtIcon} />
-          </div>
-        </div>
+            );
+          })
+        }
       </div>
 
       <div className="flex flex-col gap-7 items-center justify-center border-t-2 border-black py-44">
